51fe078bee7e48c8cc1e2cd7a720e059a523e552
Felipe Min <felipemin03@gmail.com>
2025-06-28 20:34:21 -0400

    fixed next auth provider

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { AuthProvider } from "@/components/auth-provider"
+import { AuthProvider } from "@/components/auth-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,5 +33,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-
